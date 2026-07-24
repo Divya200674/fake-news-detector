366ef789a3cd977c13e8fc2a8c24bfb2249b89ae
Divya200674 <24wh1a0574@bvrithyderabad.edu.in>
2026-07-25 00:59:25 +0530

    text-detection-updated

--- frontend/src/app/pages/text-detection/text-detection.ts
+++ frontend/src/app/pages/text-detection/text-detection.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-text-detection',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './text-detection.html',
-  styleUrl: './text-detection.css',
+  styleUrl: './text-detection.css'
 })
 export class TextDetection {}
